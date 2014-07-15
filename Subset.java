6bca247f1bcaac66d13ee6d289c4d4335b38f1bd
Girish <girish@ubuntu-jx-01.(none)>
2014-07-14 23:32:19 -0700

    Test client

---
+++ Subset.java
@@ -0,0 +1,8 @@
+public class Subset {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        StdOut.printf("k = %d\n", k);
+        StdIn.readAllStrings();
+        //StdOut.printf("s = %s\n", s);
+    }
+}
