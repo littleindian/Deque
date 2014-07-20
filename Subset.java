ef4872f2d9e083db7c5c8591557f30bb972547b3
Girish <girish@LNX-T3500.(none)>
2014-07-20 00:18:44 -0700

    Last version

--- Subset.java
+++ Subset.java
@@ -1,8 +1,15 @@
+import java.util.*;
 public class Subset {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
-        StdOut.printf("k = %d\n", k);
-        StdIn.readAllStrings();
-        //StdOut.printf("s = %s\n", s);
+        String[] s = StdIn.readAllStrings();
+        RandomizedQueue<String> randQ = new RandomizedQueue<String>();
+        for (int i = 0; i < s.length; i++)
+            randQ.enqueue(s[i]);
+        if ((k > 0) && (k <= s.length)) {
+            Iterator<String> i = randQ.iterator();
+            for (int j = 0; j < k; j++)
+                StdOut.printf("%s ", i.next());
+        }
     }
 }
